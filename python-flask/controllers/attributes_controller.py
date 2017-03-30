6e11af1fa77d894f8c36fd158e730f36dfdf78cf
chicco785 <federico.facca@gmail.com>
2017-03-30 14:30:21 +0000

    Generated code by SwaggerHub

---
+++ python-flask/controllers/attributes_controller.py
@@ -0,0 +1,9 @@
+
+def get_attribute_data(entityId, attrName, type = None) -> str:
+    return 'do some magic!'
+
+def remove_a_single_attribute(entityId, attrName, type = None) -> str:
+    return 'do some magic!'
+
+def update_attribute_data(entityId, attrName, body, type = None) -> str:
+    return 'do some magic!'
