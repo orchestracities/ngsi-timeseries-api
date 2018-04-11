e1cb2ed313bbebf000302fcebfc96b9b9da8513e
Tomas Aliaga <tomas.aliaga@gmail.com>
2018-04-11 16:26:45 +0200

    Implement multitenancy in crate translator

--- translators/crate.py
+++ translators/crate.py
@@ -395,6 +395,13 @@ class CrateTranslator(BaseTranslator):
             # Match prefix of fiware service path
             clauses.append(" "+FIWARE_SERVICEPATH+" = ''")
 
+        if fiware_sp:
+            # Match prefix of fiware service path
+            clauses.append(" "+FIWARE_SERVICEPATH+" ~* '"+fiware_sp+"($|/.*)'")
+        else:
+            # Match prefix of fiware service path
+            clauses.append(" "+FIWARE_SERVICEPATH+" = ''")
+
         where_clause = "where " + "and ".join(clauses)
         return where_clause
 
