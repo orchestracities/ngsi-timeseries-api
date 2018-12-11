1fbd0969bd563e910f3341e3c7496bae5072e20c
Andrea Falconi <andrea.falconi@gmail.com>
2018-12-11 14:11:48 +0100

    missed this file in prev commit :-)

--- src/translators/tests/test_crate_geo_query.py
+++ src/translators/tests/test_crate_geo_query.py
@@ -5,8 +5,8 @@ from geocoding.slf import *
 def test_near_min_max():
     geom = SlfPoint(1, 2)
     query = NearQuery(geometry=geom, min_distance=10, max_distance=20)
-    expected = "((distance(location_centroid, 'POINT(2.0 1.0)') >= 10) " + \
-               "and (distance(location_centroid, 'POINT(2.0 1.0)') <= 20))"
+    expected = "((distance(location_centroid, 'POINT (2.0 1.0)') >= 10) " + \
+               "and (distance(location_centroid, 'POINT (2.0 1.0)') <= 20))"
 
     assert expected == from_ngsi_query(query)
 
@@ -14,7 +14,7 @@ def test_near_min_max():
 def test_near_min():
     geom = SlfPoint(1, 2)
     query = NearQuery(geometry=geom, min_distance=10, max_distance=None)
-    expected = "(distance(location_centroid, 'POINT(2.0 1.0)') >= 10)"
+    expected = "(distance(location_centroid, 'POINT (2.0 1.0)') >= 10)"
 
     assert expected == from_ngsi_query(query)
 
@@ -22,7 +22,7 @@ def test_near_min():
 def test_near_max():
     geom = SlfPoint(1, 2)
     query = NearQuery(geometry=geom, min_distance=None, max_distance=20)
-    expected = "(distance(location_centroid, 'POINT(2.0 1.0)') <= 20)"
+    expected = "(distance(location_centroid, 'POINT (2.0 1.0)') <= 20)"
 
     assert expected == from_ngsi_query(query)
 
@@ -30,8 +30,7 @@ def test_near_max():
 def test_covered_by():
     geom = SlfPoint(1, 2)
     query = CoveredByQuery(geom)
-    expected = 'match (location, {"coordinates": [2, 1], "type": "Point"}) ' + \
-               'using within'
+    expected = "match (location, 'POINT (2 1)') using within"
 
     assert expected == from_ngsi_query(query)
 
@@ -39,8 +38,7 @@ def test_covered_by():
 def test_intersects():
     geom = SlfPoint(1, 2)
     query = IntersectsQuery(geom)
-    expected = 'match (location, {"coordinates": [2, 1], "type": "Point"}) ' + \
-               'using intersects'
+    expected = "match (location, 'POINT (2 1)') using intersects"
 
     assert expected == from_ngsi_query(query)
 
@@ -48,8 +46,7 @@ def test_intersects():
 def test_disjoint():
     geom = SlfPoint(1, 2)
     query = DisjointQuery(geom)
-    expected = 'match (location, {"coordinates": [2, 1], "type": "Point"}) ' + \
-               'using disjoint'
+    expected = "match (location, 'POINT (2 1)') using disjoint"
 
     assert expected == from_ngsi_query(query)
 
@@ -57,8 +54,7 @@ def test_disjoint():
 def test_equals():
     geom = SlfPoint(1, 2)
     query = EqualsQuery(geom)
-    expected = 'match (location, {"coordinates": [2, 1], "type": "Point"}) ' + \
-               'using within and ' + \
-               'within({"coordinates": [2, 1], "type": "Point"}, location)'
+    expected = "match (location, 'POINT (2 1)') using within and " + \
+               "within('POINT (2 1)', location)"
 
     assert expected == from_ngsi_query(query)
