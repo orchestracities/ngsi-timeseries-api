4f453f1897370681f9ec3f66ec7e17fdaa34a3c8
Andrea Falconi <andrea.falconi@gmail.com>
2019-02-11 12:44:40 +0100

    hook in new time index selection logic.

--- src/reporter/timex.py
+++ src/reporter/timex.py
@@ -57,6 +57,33 @@ def _iter_date_modified_in_metadata(notification: dict) \
 
 
 def select_time_index_value(headers: dict, notification: dict) -> datetime:
+    """
+    Determine which attribute or metadata value to use as a time index for the
+    entity being notified.
+    The returned value will be the first value found in the below list that can
+    be converted to a ``datetime``. Items are considered from top to bottom,
+    so that if multiple values are present and they can all be converted to
+    ``datetime``, the topmost value is chosen.
+
+    - Custom time index. The value of the ``TIME_INDEX_HEADER_NAME``. Note
+      that for a notification to contain such header, the corresponding
+      subscription has to be created with an ``httpCustom`` block as detailed
+      in the *Subscriptions* and *Custom Notifications* sections of the NGSI
+      spec.
+    - ``TimeInstant`` attribute.
+    - ``TimeInstant`` metadata. The most recent ``TimeInstant`` attribute value
+      found in any of the attribute metadata sections in the notification.
+    - ``dateModified`` attribute.
+    - ``dateModified`` metadata. The most recent ``dateModified`` attribute
+      value found in any of the attribute metadata sections in the notification.
+    - Current time. This is the default value we use if any of the above isn't
+      present or none of the values found can actually be converted to a
+      ``datetime``.
+
+    :param headers: the HTTP headers as received from Orion.
+    :param notification: the notification JSON payload as received from Orion.
+    :return: the value to be used as time index.
+    """
     custom_index = to_datetime(
                             _custom_time_index_attribute(headers, notification))
     time_instant = to_datetime(_time_instant_attribute(notification))
@@ -74,3 +101,11 @@ def select_time_index_value(headers: dict, notification: dict) -> datetime:
 
     return [d for d in priority_list if d][0]
     # Note. Index will never be out of bounds since we added a default value.
+
+
+def select_time_index_value_as_iso(headers: dict, notification: dict) -> str:
+    """
+    Same as ``select_time_index_value`` but formats the returned ``datetime``
+    as an ISO 8601 string.
+    """
+    return select_time_index_value(headers, notification).isoformat()
