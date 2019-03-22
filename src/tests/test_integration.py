dd23fc368cc3bad25b7de2dc6a09257603b8e169
Tomas Aliaga <tomas.aliaga@gmail.com>
2019-03-22 16:09:33 +0000

    Fix testes
    Signed-off-by: Tomas Aliaga <tomas.aliaga@gmail.com>

--- src/tests/test_integration.py
+++ src/tests/test_integration.py
@@ -2,6 +2,7 @@ from src.tests.common import load_data, check_data, unload_data
 
 
 def test_integration():
+    entities = []
     try:
         entities = load_data()
         check_data(entities)
