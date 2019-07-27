10e2429489ce60de8dc92d0fd958874e02b344af
Andrea Falconi <andrea.falconi@gmail.com>
2019-07-27 10:34:31 +0200

    make sure images are available before starting to test

--- src/translators/tests/run_tests.sh
+++ src/translators/tests/run_tests.sh
@@ -1,6 +1,9 @@
 #!/usr/bin/env bash
 
+# Make sure **all** images are available before starting to test!
 docker build -t quantumleap ../../../
+docker-compose build
+docker-compose pull --include-deps influx crate rethink timescale
 
 docker-compose up -d
 sleep 20
