8148904f3a4ac947d8bcf30a5801358aff16c3f9
Andrea Falconi <andrea.falconi@gmail.com>
2019-07-28 14:08:41 +0200

    massage translator tests to make them more travis-friendly

--- src/translators/tests/run_tests.sh
+++ src/translators/tests/run_tests.sh
@@ -2,8 +2,15 @@
 
 # Make sure **all** images are available before starting to test!
 docker build -t quantumleap ../../../
-docker-compose build
-docker-compose pull --include-deps influx crate rethink timescale
+docker-compose build quantumleap-db-setup
+docker-compose pull influx
+docker-compose pull crate
+docker-compose pull rethink
+docker-compose pull timescale
+# NOTE. The below command:
+#   docker-compose pull --include-deps influx crate rethink timescale
+# fails on Travis for some obscure reasons which is why we're pulling
+# one image at a time.
 
 docker-compose up -d
 sleep 20
