6d4d11494d9093e73edcd0c719cb60aabe72f145
Arnav Garg <arnav.garg@india.nec.com>
2019-10-10 14:32:27 +0530

    Update api.py

--- src/reporter/api.py
+++ src/reporter/api.py
@@ -1,8 +1,11 @@
 
 def list_of_api():
     return {
-                'entities_url': '/v2/entities',
-                'types_url': '/v2/types',
-                'subscriptions_url': '/v2/subscriptions',
-                'registrations_url': '/v2/registrations'
-         }
+            'config_url': '/v2/config',
+            'health_url': '/v2/health',
+            'notify_url': '/v2/notify',
+            'subscriptions_url': '/v2/subscriptions',
+            'entities_url': '/v2/entities',
+            'types_url': '/v2/types',
+            'attributes_url': '/v2/attrs'
+    }
