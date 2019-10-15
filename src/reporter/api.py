f59430b88ef39e4b68fe594e6b5d2940a4a2dc24
Damini-NEC <damini.c@india.nec.com>
2019-10-15 15:40:09 +0530

    Update api.py

--- src/reporter/api.py
+++ src/reporter/api.py
@@ -1,9 +1,9 @@
 
 def list_of_api():
     return {
-            'config_url': '/v2/config',
-            'health_url': '/v2/health',
-            'notify_url': '/v2/notify',
+            'config_url': '/config',
+            'health_url': '/health',
+            'notify_url': '/notify',
             'subscriptions_url': '/v2/subscriptions',
             'entities_url': '/v2/entities',
             'types_url': '/v2/types',
