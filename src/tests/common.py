32d34909b5ad09608e81c84dca2cf37a76e2748e
Damini-NEC <damini.c@india.nec.com>
2019-10-16 10:26:34 +0530

    Update common.py

--- src/tests/common.py
+++ src/tests/common.py
@@ -186,7 +186,7 @@ def unload_data(entities):
         h = e.headers()
         if e.FIWARE_SERVICEPATH_KEY in h:
             h.pop(e.FIWARE_SERVICEPATH_KEY)
-        url = "{}/types/{}".format(QL_URL, e.type)
+        url = "{}/v2/types/{}".format(QL_URL, e.type)
 
         r = requests.delete(url, headers=h)
         if not r.ok:
