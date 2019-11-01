c2d0d8ec0df89193e329b4504b6feeb263d4ab9b
Andrea Falconi <andrea.falconi@gmail.com>
2019-11-01 13:05:33 +0100

    output debug info

--- src/reporter/tests/run_tests.sh
+++ src/reporter/tests/run_tests.sh
@@ -7,21 +7,29 @@ sleep 12
 
 cd ../../../
 
+echo '=====================> I/F'
+ip a
+
 echo '=====================> localhost'
+ping -c 1 localhost
 curl http://localhost:8668/v2/version
 
 echo '=====================> 127.0.0.1'
+ping -c 1 127.0.0.1
 curl http://127.0.0.1:8668/v2/version
 
 echo '=====================> 0.0.0.0'
+ping -c 1 0.0.0.0
 curl http://0.0.0.0:8668/v2/version
 
 h=$(hostname)
 echo "=====================> ${h}"
+ping -c 1 ${h}
 curl "http://${h}:8668/v2/version"
 
 ip=$(hostname -I | awk '{print $1}')
 echo "=====================> ${ip}"
+ping -c 1 ${ip}
 curl "http://${ip}:8668/v2/version"
 
 
