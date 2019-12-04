8f4ddd4e2d9dec221e0f13d2d470fd5fd0a95d23
Pooja <pooja@nectechnologies.in>
2019-12-04 16:58:38 +0530

    updated code as per comment

--- src/reporter/query_NTNE.py
+++ src/reporter/query_NTNE.py
@@ -4,8 +4,10 @@ from reporter.reporter import _validate_query_params
 from translators.crate import CrateTranslatorInstance, CrateTranslator
 import logging
 
-
 def query_NTNE(limit=10000,
+                 type_=None,  # In Query
+                 from_date=None,
+                 to_date=None,
                  offset=0):
     """
     See /entities in API Specification
@@ -17,15 +19,17 @@ def query_NTNE(limit=10000,
     entities = None
     try:
         with CrateTranslatorInstance() as trans:
-            entities = trans.query(limit=limit,
+            entities = trans.query_ids(limit=limit,
+                                   entity_type=type_,
+                                   from_date=from_date,
+                                   to_date=to_date,
                                    offset=offset,
                                    fiware_service=fiware_s,
                                    fiware_servicepath=fiware_sp)
     except NGSIUsageError as e:
-        return {
-            "error": "{}".format(type(e)),
-            "description": str(e)
-        }, 400
+        msg = "Bad Request Error: {}".format(e)
+        logging.getLogger().error(msg, exc_info=True)
+        return msg, 400
 
     except Exception as e:
         msg = "Internal server Error: {}".format(e)
@@ -33,13 +37,10 @@ def query_NTNE(limit=10000,
         return msg, 500
 
     if entities:
-        ids = []
+        res = []
         for entity in entities:
-          ids.append(entity['id'])
+          res.append(entity)
 
-        res = {
-            'entityId': ids
-        }
         return res
 
     r = {
@@ -47,3 +48,4 @@ def query_NTNE(limit=10000,
         "description": "No records were found for such query."
     }
     return r, 404
+
--- src/reporter/tests/test_NTNE.py
+++ src/reporter/tests/test_NTNE.py
@@ -5,7 +5,10 @@ import pytest
 import requests
 
 entity_type = 'Room'
+entity_type_1 = 'Kitchen'
 entity_id = 'Room0'
+entity_id_1 = 'Kitchen0'
+n_days = 30
 
 def query_url():
     url = "{qlUrl}/entities"
@@ -16,20 +19,32 @@ def query_url():
 
 @pytest.fixture()
 def reporter_dataset(translator):
-    insert_test_data(translator, [entity_type], n_entities=1, index_size=30)
+    insert_test_data(translator, [entity_type], n_entities=1, index_size=30, entity_id=entity_id)
+    insert_test_data(translator, [entity_type_1], n_entities=1, index_size=30, entity_id=entity_id_1)
     yield
 
 def test_NTNE_defaults(reporter_dataset):
-
     r = requests.get(query_url())
     assert r.status_code == 200, r.text
 
     obtained = r.json()
-    exp_values = ['Room0']
+    exp_values = [{
+        "id": 'Kitchen0',
+        "index": [
+            "1970-01-30T00:00:00.000"
+        ],
+        "type": 'Kitchen'
+    },
+    {
+        "id": 'Room0',
+        "index": [
+            "1970-01-30T00:00:00.000"
+        ],
+        "type": 'Room'
+    }]
 
-    expected = {
-        'entityId': exp_values
-    }
+    expected = exp_values
+
     assert obtained == expected
 
 def test_not_found():
@@ -39,3 +54,144 @@ def test_not_found():
         "error": "Not Found",
         "description": "No records were found for such query."
     }
+
+def test_NTNE_type(reporter_dataset):
+    # Query
+    query_params = {
+        'type': entity_type
+    }
+    r = requests.get(query_url(), params=query_params)
+    assert r.status_code == 200, r.text
+
+    # Assert
+    obtained = r.json()
+    expected_type = 'Room'
+    expected_values = list(range(n_days))
+    expected_index = [
+        '1970-01-{:02}T00:00:00'.format(i+1) for i in expected_values
+    ]
+    expected = [{
+        'id': 'Room0',
+        'index': [expected_index[-1]+'.000'],
+        'type': expected_type
+    }]
+    assert obtained == expected
+
+def test_NTNE_fromDate_toDate(reporter_dataset):
+    # Query
+    query_params = {
+        'fromDate': "1970-01-06T00:00:00",
+        'toDate': "1970-01-17T00:00:00",
+    }
+    r = requests.get(query_url(), params=query_params)
+    assert r.status_code == 200, r.text
+
+    # Expect only last N
+    expected_values = list(range(5, 17))
+    expected_type = 'Room'
+    expected_id = 'Room0'
+    expected_type_1 = 'Kitchen'
+    expected_id_1 = 'Kitchen0'
+    expected_index = [
+        '1970-01-{:02}T00:00:00.000'.format(i+1) for i in expected_values
+    ]
+    assert len(expected_index) == 12
+    assert expected_index[0] == "1970-01-06T00:00:00.000"
+    assert expected_index[-1] == "1970-01-17T00:00:00.000"
+
+    # Assert
+    obtained = r.json()
+    expected = [{
+        'id': expected_id_1,
+        'index': [expected_index[-1]],
+        'type': expected_type_1
+    },
+    {
+        'id': expected_id,
+        'index': [expected_index[-1]],
+        'type': expected_type
+    }]
+    assert obtained == expected
+
+def test_NTNE_limit(reporter_dataset):
+    # Query
+    query_params = {
+        'limit': 1
+    }
+    r = requests.get(query_url(), params=query_params)
+    assert r.status_code == 200, r.text
+
+    expected_type = 'Room'
+    expected_id = 'Room0'
+    expected_index = [
+        '1970-01-30T00:00:00.000'
+    ]
+
+    # Assert
+    obtained = r.json()
+    expected = [{
+        'id': expected_id,
+        'index': expected_index,
+        'type': expected_type
+    }]
+    assert obtained == expected
+
+def test_NTNE_offset(reporter_dataset):
+    # Query
+    query_params = {
+        'offset': 1
+    }
+    r = requests.get(query_url(), params=query_params)
+    assert r.status_code == 200, r.text
+
+    # Expect only last N
+    expected_values = list(range(1, n_days))
+    expected_type = 'Kitchen'
+    expected_id = 'Kitchen0'
+    expected_index = [
+        '1970-01-{:02}T00:00:00'.format(i+1) for i in expected_values
+    ]
+    assert len(expected_index) == 29
+    assert expected_index[0] == "1970-01-02T00:00:00"
+    assert expected_index[-1] == "1970-01-30T00:00:00"
+
+    # Assert
+    obtained = r.json()
+    expected = [{
+        'id': expected_id,
+        'index': [expected_index[-1]+'.000'],
+        'type': expected_type
+    }]
+    assert obtained == expected
+
+def test_NTNE_combined(reporter_dataset):
+    # Query
+    query_params = {
+        'type': entity_type,
+        'offset': 0,
+        'toDate': "1970-01-20T00:00:00",
+        'limit': 1,
+    }
+    r = requests.get(query_url(), params=query_params)
+    assert r.status_code == 200, r.text
+
+    # Expect only last N
+    expected_values = list(range(0, 20))
+    expected_type = 'Room'
+    expected_id = 'Room0'
+    expected_index = [
+        '1970-01-{:02}T00:00:00'.format(i+1) for i in expected_values
+    ]
+    assert len(expected_index) == 20
+    assert expected_index[0] == "1970-01-01T00:00:00"
+    assert expected_index[-1] == "1970-01-20T00:00:00"
+
+    # Assert
+    obtained = r.json()
+    expected = [{
+        'id': expected_id,
+        'index': [expected_index[-1]+'.000'],
+        'type': expected_type
+    }]
+    assert obtained == expected
+
