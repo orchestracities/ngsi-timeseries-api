8e7f8a1c8ef7d412f4b58cab4fa4da5346cf212e
Pooja Pathak <33315652+pooja1pathak@users.noreply.github.com>
2020-01-27 16:36:06 +0530

    Update query_1TNE1A.py

--- src/reporter/query_1TNE1A.py
+++ src/reporter/query_1TNE1A.py
@@ -40,6 +40,11 @@ def query_1TNE1A(attr_name,   # In Path
 
     fiware_s = request.headers.get('fiware-service', None)
     fiware_sp = request.headers.get('fiware-servicepath', None)
+    
+    if from_date:
+        from_date = from_date.strip('\"')
+    if to_date:
+        to_date = to_date.strip('\"')
 
     entities = None
     entity_ids = None
