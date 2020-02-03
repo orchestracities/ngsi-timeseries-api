a9c7c7b1fb6d2cb6ebea08e8790205f3f3d2446f
pooja1pathak <pooja@india.nec.com>
2020-02-03 14:42:34 +0530

    updated code for delete_entities

--- src/reporter/delete.py
+++ src/reporter/delete.py
@@ -35,11 +35,6 @@ def delete_entity(entity_id, type_=None, from_date=None, to_date=None):
 def delete_entities(entity_type, from_date=None, to_date=None):
     fiware_s = request.headers.get('fiware-service', None)
     fiware_sp = request.headers.get('fiware-servicepath', None)
-    
-    if from_date:
-        from_date = from_date.strip('\"')
-    if to_date:
-        to_date = to_date.strip('\"')
 
     with crate.CrateTranslatorInstance() as trans:
         deleted = trans.delete_entities(entity_type=entity_type,
