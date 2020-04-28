54fa119a040d95e50fb1e4e1973525c5d2422363
Christoph Nölle <19640096+cnoelle@users.noreply.github.co>
2020-04-29 00:40:59 +0200

    cors test setup

--- src/tests/run_tests_cors.sh
+++ src/tests/run_tests_cors.sh
@@ -1,18 +1,18 @@
 #!/usr/bin/env bash
 
 # Build Docker Image
-docker build -t quantumleap ../../
+docker build -t quantumleap_test -f ./../../Dockerfile_test ./../../
 source ./../../deps.env
 
 # Launch services
-QL_IMAGE=quantumleap docker-compose up -d
+docker-compose -f docker-compose-test.yml up -d
 sleep 12
 
 # Run CORS tests in quantumleap docker container
 docker exec tests_quantumleap_1 sh -c \
-  "QL_URL=http://quantumleap:8668 ORION_URL=http://orion:1026 pytest tests/test_cors.py --cov-report= --cov-config=.coveragerc --cov-append --cov=./"
+  "QL_URL=http://localhost:8668 ORION_URL=http://orion:1026 pytest tests/test_cors.py --cov-report= --cov-config=.coveragerc --cov-append --cov=./"
 tot=$?
 
-docker-compose down
+docker-compose -f docker-compose-test.yml down
 echo ${tot}
 exit ${tot}
