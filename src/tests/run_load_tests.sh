a13a8f5192cee9cd902c05bb194e15092b4672fd
Andrea Falconi <andrea.falconi@gmail.com>
2020-09-03 15:40:35 +0200

    port notify load test from #354.

---
+++ src/tests/run_load_tests.sh
@@ -0,0 +1,16 @@
+#!/usr/bin/env bash
+
+docker-compose up -d
+docker-compose stop orion
+docker-compose stop mongo
+sleep 10
+
+docker run -i loadimpact/k6 run --vus 10 --duration 60s - < notify-load-test.js
+
+sleep 10
+
+docker run -i loadimpact/k6 run --vus 100 --duration 120s - < notify-load-test.js
+
+sleep 10
+
+docker-compose down
