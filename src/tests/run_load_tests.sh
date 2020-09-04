960a148347f3e77859b0a1f6470129ba7c3599fb
Andrea Falconi <andrea.falconi@gmail.com>
2020-09-04 14:16:25 +0200

    make sure load test uses latest ql code and removes containers to avoid undeletable dangling images.

--- src/tests/run_load_tests.sh
+++ src/tests/run_load_tests.sh
@@ -1,15 +1,17 @@
 #!/usr/bin/env bash
 
+docker build --cache-from smartsdk/quantumleap -t smartsdk/quantumleap ../../
+
 docker-compose up -d
 docker-compose stop orion
 docker-compose stop mongo
 sleep 10
 
-docker run -i loadimpact/k6 run --vus 10 --duration 60s - < notify-load-test.js
+docker run -i --rm loadimpact/k6 run --vus 10 --duration 60s - < notify-load-test.js
 
 sleep 10
 
-docker run -i loadimpact/k6 run --vus 100 --duration 120s - < notify-load-test.js
+docker run -i --rm loadimpact/k6 run --vus 100 --duration 120s - < notify-load-test.js
 
 sleep 10
 
