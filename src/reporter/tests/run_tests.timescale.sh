06e963d14cff64bb580d4c0bb994563b77538834
Andrea Falconi <andrea.falconi@gmail.com>
2020-09-28 15:13:22 +0200

    exclude from timescale reporter suite test modules that still dont work with timescale.

--- src/reporter/tests/run_tests.timescale.sh
+++ src/reporter/tests/run_tests.timescale.sh
@@ -14,8 +14,19 @@ cd ../../../
 # TODO: comment in above and zap line below when Timescale backend
 # is fully functional.
 
-pytest src/reporter/tests/_tmptest_delete_with_timescale.py \
-       --cov-report= --cov-config=.coveragerc --cov-append --cov=src/
+pytest src/reporter/ \
+       --cov-report= --cov-config=.coveragerc --cov-append --cov=src/ \
+       --ignore=src/reporter/tests/test_geo_queries_1t1e.py \
+       --ignore=src/reporter/tests/test_geo_query_1tne1a.py \
+       --ignore=src/reporter/tests/test_health.py \
+       --ignore=src/reporter/tests/test_incomplete_entities.py \
+       --ignore=src/reporter/tests/test_integration.py \
+       --ignore=src/reporter/tests/test_multitenancy.py \
+       --ignore=src/reporter/tests/test_notify.py \
+       --ignore=src/reporter/tests/test_sql_injection.py \
+       --ignore=src/reporter/tests/test_subscribe.py \
+       --ignore=src/reporter/tests/test_time_format.py
+
 r=$?
 cd -
 
@@ -23,3 +34,20 @@ unset POSTGRES_PORT
 
 docker-compose -f docker-compose.timescale.yml down -v
 exit $r
+
+# NOTE. Failing tests.
+# * test_geo*: Timescale geo-queries not implemented yet.
+# * test_health: Endpoint hardcoded to work w/ Crate only.
+# * test_incomplete_entities: ??
+# * test_integration: works w/ Crate, possibly not needed for Timescale.
+# * test_multitenancy: ditto.
+# * test_notify: test_no_value_no_type_for_attributes is broken but it
+#   looks like the test was wrong to start with. In fact, the test checks
+#   (among other things) that a numeric attribute (temperature) with a string
+#   value ('25') is inserted in Crate with a text column type. The Timescale
+#   backend inserts it as float, which is correct?
+# * test_sql_injection: needs some massaging to make it work with Timescale
+#   too.
+# * test_subscribe: ditto.
+# * test_time_format: in principle these tests should be able to work with
+#   both backends but during test setup we connect to Crate.
