b5ac91c8912584c4ee0265903859ef4eebf76c40
Federico M. Facca <federico.facca@gmail.com>
2021-03-06 18:22:05 +0100

    output steps

--- setup_dev_env.sh
+++ setup_dev_env.sh
@@ -10,15 +10,25 @@ export PYTHONPATH=${PWD}/src:${PYTHONPATH}
 
 docker build -t orchestracities/quantumleap .
 
+
+echo "build"
+
 source deps.env
 
+
+echo "source"
+
 LH=`( /sbin/ifconfig ens4 | grep 'inet' | cut -d: -f2 | awk '{ print $1}' ) 2> /dev/null`
+echo "lh"
+echo $LH
 if [ -z "$LH" ]
 then
     # Aliasing so that notifications from orion container reach dev localhost
     LH=192.0.0.1
     sudo ifconfig lo0 alias $LH
 fi
+echo "export"
+echo $LH
 
 export ORION_HOST=$LH
 export MONGO_HOST=$LH
@@ -28,5 +38,7 @@ export CRATE_HOST=$LH
 export POSTGRES_HOST=$LH
 
 export REDIS_HOST=$LH
+
+echo "complete!"
 
 [[ "$LH" != "192.0.0.1" ]] || pipenv shell
