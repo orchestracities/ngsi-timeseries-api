6271c389ca5f69c94410e6ccb3769f1d0ef93e3b
Federico M. Facca <federico.facca@gmail.com>
2021-03-07 11:39:38 +0100

    ip fix for circleci

--- setup_dev_env.sh
+++ setup_dev_env.sh
@@ -12,7 +12,8 @@ source deps.env
 
 if ! command -v /sbin/ifconfig &> /dev/null
 then
-    LH=192.0.0.1
+    ip address show
+    LH=127.0.0.1
 else
   LH=`( /sbin/ifconfig ens4 | grep 'inet' | cut -d: -f2 | awk '{ print $1}' ) 2> /dev/null`
   if [ -z "$LH" ]
