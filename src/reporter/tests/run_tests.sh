1cfdca1fa38c714ee4b85300877bf34064388c99
Federico M. Facca <chicco785@users.noreply.github.com>
2021-08-18 13:58:44 +0200

    clean up (#529)
    * Clean up old code
    * comment out '/server' path from coveralls
    * migrate to vegeta for load tests (fix #415)
    * add ql_config for reporter tests (otherwise only crate is tested!)

--- src/reporter/tests/run_tests.sh
+++ src/reporter/tests/run_tests.sh
@@ -7,6 +7,9 @@ docker-compose pull redis
 docker-compose up -d
 sleep 20
 
+# Set test QL config file
+export QL_CONFIG='src/reporter/tests/ql-config.yml'
+
 cd ../../../
 pytest src/reporter/ \
        --cov-report= --cov-config=.coveragerc --cov-append --cov=src/ \
