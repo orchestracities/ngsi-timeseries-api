acb45f943e370cc5debae8c8ffdbb11423250031
Federico M. Facca <federico.facca@gmail.com>
2021-08-31 01:11:34 +0200

    fix project names for lovely docker

--- src/geocoding/tests/conftest.py
+++ src/geocoding/tests/conftest.py
@@ -6,3 +6,8 @@ import os
 def docker_redis(docker_services):
     os.environ['PATH'] += os.pathsep + "/usr/local/bin"
     docker_services.start('redis')
+
+
+@pytest.fixture(scope='session')
+def docker_services_project_name():
+    return "geocoding-test"
