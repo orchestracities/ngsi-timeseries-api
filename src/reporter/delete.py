1402f44e0c362cbe80543a855f23dd42c7229dd7
Damini <damini.c@india.nec.com>
2021-09-02 22:17:34 +0530

    Drop table enhancement (#455)
    * improve drop table logic
      1. if request has no service path ==> drop table
      2.  if request has one or many service paths ==> 422 error
    * add tests for the new logic

--- src/reporter/delete.py
+++ src/reporter/delete.py
@@ -34,11 +34,17 @@ def delete_entity(entity_id, type_=None, from_date=None, to_date=None):
 def delete_entities(entity_type, from_date=None, to_date=None,
                     drop_table=False):
     with translator_for(fiware_s()) as trans:
-        if drop_table:
-            trans.drop_table(etype=entity_type, fiware_service=fiware_s())
+        if drop_table and fiware_sp() is None:
+            trans.drop_table(etype=entity_type,
+                             fiware_service=fiware_s())
             logging.getLogger(__name__).info(
                 "dropped entity_type {}".format(entity_type))
             return 'entity table dropped', 204
+        elif drop_table:
+            e = fiware_sp().split(",")
+            if e is not None and len(e) > 0:
+                return "request has one or more fiware service paths," \
+                       "dropTable requires no service path", 422
 
         deleted = trans.delete_entities(etype=entity_type,
                                         from_date=from_date,
