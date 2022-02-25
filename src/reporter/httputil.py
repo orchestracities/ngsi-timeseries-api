d08282983f40ee530dcd1a468b22068c0ed857de
pooja1pathak <pooja@india.nec.com>
2022-02-25 05:50:31 +0000

    updated as per comment

--- src/reporter/httputil.py
+++ src/reporter/httputil.py
@@ -20,3 +20,8 @@ def fiware_correlator() -> str:
     :return: The content of the FIWARE correlator path header if any.
     """
     return request.headers.get('Fiware-Correlator', None)
+
+def is_root_service_path() -> bool:
+    sp = fiware_sp() or ''  # cater for header not present
+    paths = {*[path.strip() for path in sp.split(',')]}  # cater for whitespace around paths
+    return len(paths) == 1 and '/' in paths
